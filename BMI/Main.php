61536bc86e43d9706ac1ed1940e32215fafe6397
Himanshu Srivastav <96463814+himanshu562001@users.noreply.github.com>
2024-04-26 11:00:08 +0530

    Update Main.php

--- BMI/Main.php
+++ BMI/Main.php
@@ -10,13 +10,13 @@
             <form method="post">
                 <h1>BMI Calculator</h1>
                 <label>Enter the height:</label><br>
-                <input type="number" name="num"/><br>
+                <input type="number" name="height"/><br>
                 <select name="unit1">
                     <option>feet</option>
                     <option>centimeter</option>
                 </select><br/>
                 <label>Enter the Weight:</label><br>
-                <input type="number" name="num2"/><br>
+                <input type="number" name="weight"/><br>
                 <select name="unit2">
                     <option>kg</option>
                     <option>pound</option>
@@ -31,60 +31,63 @@
          </form>
          <?php
          error_reporting(0);
-         print_r($_POST);
+        // print_r($_POST);
          if(isset ($_POST['Submit'])){ 
-             if(isset($_POST['num']) && isset( $_POST['num2']) )  
+             if(isset($_POST['height']) && isset( $_POST['weight']) )  
 
              {
                 
-                $weight= $_POST['num'];
-                $height= $_POST['num2'];
+                $height= $_POST['height'];
+                $weight= $_POST['weight'];
                 $unit1= $_POST['unit1'];
                 $unit2= $_POST['unit2'];
+
+              
 
   
                 if($unit1=="feet"){
                     $height= $height*0.3048;
-                 
+                    
                 }
+                
                  if($unit1== "centimeter"){
 
                     $height= $height*0.01;
                 
              }
              
-             if($unit2== "kg"){
+            /* if($unit2== "kg"){
 
                 $weight= $kg;
-            }
+            }*/
                
                 if($unit2== "pound"){
 
                     $weight= $weight*0.453;
                 }
+       
+
                     $result= $weight/($height*$height);
+                   
 
-                    echo $result."<br/>";
 
-                    echo "BMI". number_format($result,0);
+                    echo "BMI". "<br/>". number_format($result,0);
 
                     if($result<=18){
-                        echo 'underweight'."<br/>";
+                        echo "<br/>".'Underweight';
 
                     }
-                    else if($result>=19 || $result<=24){
-                        echo "Normal";
+                     if($result>=19 || $result<=24){
+                        echo "<br/>"."Normal";
 
                     }
-                    else if($result>= 25|| $result<= 29){
 
-                        echo "Overweight"; 
-                        
+                   if($result>=25 || $result<=29){
+                        echo "<br/>"."Overweight";
 
                     }
-                    else{
-                    echo "Enter weight and height";
-                    }
+                               
+                   
                 }
 
              }
